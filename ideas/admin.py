fe76e78f55f6070e5f0f3101d9dd85bddb639487
saleeja <saleejas2@gmail.com>
2023-07-20 13:53:14 +0530

    commit

---
+++ ideas/admin.py
@@ -0,0 +1,13 @@
+Coursefrom django.contrib import admin
+from .models import Course
+from .models import Technology
+from.models import Project_ideas
+
+
+
+
+# Register your models here.
+
+admin.site.register(Course)
+admin.site.register(Technology)
+admin.site.register(Project_ideas)
